1ade3f612fc38db9baa694a567509b16f1ece5cc
poccariswet <s1240231@gmail.com>
2018-10-29 00:23:52 +0900

    [FIX] added created at time

--- storage/storage.go
+++ storage/storage.go
@@ -1,7 +1,10 @@
 package storage
 
+import "time"
+
 type UrlSho struct {
-	ID      string `json:"id" redis:"id"`
-	LongURL string `json:"long_url" redis:"long_url"`
-	Count   int    `json:"count" redis:"count"`
+	ID        string    `json:"id" redis:"id"`
+	LongURL   string    `json:"long_url" redis:"long_url"`
+	Count     int       `json:"count" redis:"count"`
+	CreatedAt time.Time `json:"createdAt" redis:"createdAt"`
 }
