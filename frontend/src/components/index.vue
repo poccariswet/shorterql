4ce3e3fc1d35cc4fd49e2d505a987ec807d2cd27
poccariswet <s1240231@gmail.com>
2019-01-21 16:58:38 +0900

    [ADD] index page

---
+++ frontend/src/components/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h1>url shortener</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
