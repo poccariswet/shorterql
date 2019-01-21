8527e133b160f7d97df03674627fb2e383ae4b65
soeyu <s1240231@gmail.com>
2019-01-21 17:26:32 +0900

    front screen init (#16)
    * [ADD] init front
    
    * [ADD] index page
    
    * [ADD] input url for now

---
+++ frontend/src/components/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <h1>url shortener</h1>
+    <input>
+    <button></button>
+    <!--showing short url on popup -->
+    <!-- list of the last 10 short url under input -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
