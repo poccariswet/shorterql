c6ba331dfd6c870ac2d77868a42b1bd33d5084d5
poccariswet <s1240231@gmail.com>
2019-01-21 17:24:42 +0900

    [ADD] input url for now

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <h1>url shortener</h1>
+    <input>
+    <button></button>
+    <!--showing short url on popup -->
+    <!-- list of the last 10 short url under input -->
   </div>
 </template>
 
